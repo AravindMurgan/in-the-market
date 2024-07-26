616b80cc6d67b1b3f667e6c8804984750b41d3a5
AravindMurugan <mi.arvindmurugan@gmail.com>
2024-07-26 22:58:51 +0100

    DB connection added

---
+++ config/database.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+let connected =false
+
+const connectDB =async()=>{
+    mongoose.set('strictQuery',true);
+
+
+    //If the DB is already connected, dont connect it again
+    if(connected){
+        console.log("MongoDB is already connected")
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        connected =true
+        console.log("Mongo DB Connected")
+
+    } catch (error) {
+        console.log("ERROR::::")
+        console.log(error)
+    }
+
+
+}
+
+
+export default connectDB
