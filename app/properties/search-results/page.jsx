97e2dcdb1c87b9e7130c188597be7a9336448999
AravindMurugan <mi.arvindmurugan@gmail.com>
2024-09-03 19:59:25 +0100

    Modified to client component !

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,10 +1,15 @@
+ "use client";
 import Link from 'next/link';
 import { FaArrowAltCircleLeft } from 'react-icons/fa';
 import PropertyCard from '@/components/PropertyCard';
 import PropertySearchForm from '@/components/PropertySearchForm';
-import connectDB from '@/config/database';
-import Property from '@/models/Property';
-import { convertToSerializeableObject } from '@/utils/convertToObject';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import getPropertiesQueryResults from '@/app/actions/getPropertiesQueryResults';
+import { Checkbox } from '@nextui-org/react';
+
+
+// import getPropertiesQueryResults from '@/app/actions/getpropertiesQueryResults';
 
 // NOTE: This component has been changed to a server component where we can
 // query the database directly.
@@ -13,44 +18,29 @@ import { convertToSerializeableObject } from '@/utils/convertToObject';
 // Moving this component to a server component means we can remove our app/api/properties/search/route.js
 // route handler as it's no longer used.
 
-const SearchResultsPage = async ({
-  searchParams: { location, zipCode },
-}) => {
-  await connectDB();
+function SearchResultsPage() {
+  const searchParams = useSearchParams();
+  const location = searchParams.get('location') || '';
+  const zipCode = searchParams.get('zipCode') || '';
 
-  const locationPattern = new RegExp(location, 'i');
-const zipCodePattern = zipCode.length > 0 ? new RegExp(zipCode, 'i') : null;
+  const [properties, setProperties] = useState([]);
 
-let query = {};
+  useEffect( () => { 
+    async function getPropertiesQueryResultsData(){
+      const propertiesQueryResults = await getPropertiesQueryResults(location, zipCode);
+      // const propertiesData = convertToSerializeableObject(propertiesQueryResults);
+      // console.log(propertiesData);
+      console.log(propertiesQueryResults)
+      setProperties(propertiesQueryResults);
+  
+    }
+    getPropertiesQueryResultsData();
 
-// Match location pattern against database fields
-query = {
-  $or: [
-    {
-      $or: [
-        { name: locationPattern },
-        { description: locationPattern },
-        { 'location.street': locationPattern },
-        { 'location.city': locationPattern },
-        { 'location.state': locationPattern },
-      ],
-    },
-  ],
-};
+  }, [location,zipCode]);
 
-if(zipCodePattern){
-  query.$or.unshift({ 'location.zipcode': zipCodePattern });
-}
-
-  // // Only check for property if its not 'All'
-  // if (propertyType && propertyType !== 'All') {
-  //   const typePattern = new RegExp(propertyType, 'i');
-  //   query.type = typePattern;
+  // if(!isMounted){
+  //   return null;
   // }
-
-  const propertiesQueryResults = await Property.find(query).lean();
-  const properties = convertToSerializeableObject(propertiesQueryResults);
-
   return (
     <>
       <section className='bg-blue-700 py-4'>
@@ -70,15 +60,28 @@ if(zipCodePattern){
           {properties.length === 0 ? (
             <p>No search results found</p>
           ) : (
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-              {properties.map((property) => (
-                <PropertyCard key={property._id} property={property} />
-              ))}
-            </div>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+            {/* Sidebar */}
+            <aside className="col-span-1 bg-gray-100 p-4">
+              <h2 className="text-xl mb-4">Filters</h2>
+              {/* Add your filter components here */}
+              <p>Filter options go here</p>
+              <Checkbox defaultSelected>Option</Checkbox>
+            </aside>
+  
+            {/* Main Content */}
+            <main className="col-span-3">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {properties.map((property) => (
+                  <PropertyCard key={property._id} property={property} />
+                ))}
+              </div>
+            </main>
+          </div>
           )}
         </div>
       </section>
     </>
   );
-};
+}
 export default SearchResultsPage;
